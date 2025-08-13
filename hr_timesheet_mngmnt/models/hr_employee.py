a8f39ef46623b05c9e83e8693b7b8340c4a921ef
IliaYusov <ouster2k@gmail.com>
2025-08-13 16:19:13 +0300

    rollback employee hourly_cost due to tenders error

--- hr_timesheet_mngmnt/models/hr_employee.py
+++ hr_timesheet_mngmnt/models/hr_employee.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    hourly_cost = fields.Monetary(string='Hourly Cost', default=0.0)
+    # hourly_cost = fields.Monetary(string='Hourly Cost', default=0.0)
